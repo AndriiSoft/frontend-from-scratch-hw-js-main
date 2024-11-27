69bd584ca374efa454525d2a051c926080d4197c
Andrii Bondar <bondar_andriy@ukr.net>
2024-11-27 18:01:17 +0100

    task #1

---
+++ lesson-01/01.js
@@ -0,0 +1,13 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let sum = 0;
+let count = 0;
+
+while (count <= 20) {
+    sum = sum + count;
+    count++;
+}
+//console.log(sum);
+
