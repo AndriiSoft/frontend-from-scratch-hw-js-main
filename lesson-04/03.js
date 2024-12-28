2807a16df045ef4e897795afe87c5f45aa22c89d
Andrii Bondar <bondar_andriy@ukr.net>
2024-12-28 08:57:39 +0100

    JS lesson 4 task 3

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -23,7 +23,7 @@ function findCommonElements(array1, array2) {
     const newArray = []
     for (let i = 0; i < array1.length; i++) {
         for (let j = 0; j < array2.length; j++) {
-            if (array1[i] === array2[j]) {
+            if (array1[i] === array2[j] && !newArray.includes(array1[i])){
                 newArray.push(array1[i]);
             }
         }
